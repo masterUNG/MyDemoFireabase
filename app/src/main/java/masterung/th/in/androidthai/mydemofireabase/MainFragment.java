84afe37e0ef572dd4411af319b862d295a2070a5
masterUNG <phrombutr@gmail.com>
2018-12-06 14:04:50 +0700

    Design UI Register Finish

--- app/src/main/java/masterung/th/in/androidthai/mydemofireabase/MainFragment.java
+++ app/src/main/java/masterung/th/in/androidthai/mydemofireabase/MainFragment.java
@@ -2,10 +2,12 @@ package masterung.th.in.androidthai.mydemofireabase;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -16,14 +18,42 @@ public class MainFragment extends Fragment {
 
     public MainFragment() {
         // Required empty public constructor
-    }
+    }   // Constructor
 
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        SignUp Controller
+        signUpController();
+
+
+    }   // Main Method
+
+    private void signUpController() {
+        Button button = getView().findViewById(R.id.btnSignUp);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                Replace Fragment
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
-    }
+    }   // onCreateView
 
-}
+}   // Main Class
